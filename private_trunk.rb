03a4b8b7ee1c5ddd92560a9edab396a993ee2f57
Cloud <cloudcry@gmail.com>
2015-07-25 00:48:49 +0800

    add auto create new tag and push it to remote

--- private_trunk.rb
+++ private_trunk.rb
@@ -7,7 +7,7 @@ EXEC_FILE_NAME = File.basename $0
 EXEC_EXT_NAME = File.extname $0
 EXEC_NAME = File.basename($0, EXEC_EXT_NAME)
 COMMAND_NAME_SUFFIX = "_trunk"
-VERSION = "0.1"
+VERSION = "0.2"
 
 def install
     puts "Install ......"
@@ -28,14 +28,15 @@ def repo_name_from_exec_name
     pieces.join '_'
 end
 
-def update_repo_ok? repo_name
+def update_repo repo_name
     update_result = `pod repo update #{repo_name}`
     puts update_result
     if update_result.start_with? "[!] Unable to find the"
         puts "ERR:: The '#{repo_name}' is not existed. Use 'pod repo add <name> <repo url>' to add it."
-        false
     else
-        true
+        if block_given?
+            yield repo_name
+        end
     end
 end
 
@@ -45,26 +46,27 @@ def parse_spec_name_and_version_from_podspec file_path
     require 'json'
     podspec_json = JSON.parse podspec_json_string
     if podspec_json and podspec_json["name"] and podspec_json["version"]
-        [podspec_json["name"], podspec_json["version"]]
+        if block_given?
+            yield podspec_json["name"], podspec_json["version"]
+        end
     else
         puts "ERR:: Parse podspec json string fail."
-        [nil, nil]
     end
 end
 
-def podspec_file_path
+def get_podspec_file_path
     file_path = Dir.glob("./*.podspec").first
     if file_path.nil? or file_path.empty?
         puts "ERR:: podspec file does not exist."
-        nil
     else
-        file_path
+        if block_given?
+            yield file_path
+        end
     end
 end
 
 def commit_and_push(file_path, repo_spec_path, message)
     puts "Start push the new version podspec ......"
-    require 'fileutils'
     FileUtils.mkdir_p repo_spec_path
     FileUtils.cp file_path, repo_spec_path
     FileUtils.cd(repo_spec_path) do
@@ -77,23 +79,48 @@ def version
     VERSION
 end
 
+def tag_exist_on_remote? spec_version
+    result = `git ls-remote --tags origin refs/tags/#{spec_version}`
+    not (result.nil? or result.empty?)
+end
+
+def tag_exist_in_local? spec_version
+    result = `git tag -l #{spec_version}`
+    not (result.nil? or result.empty?)
+end
+
+def push_spec_version_tag spec_version
+    if tag_exist_on_remote? spec_version
+        if block_given?
+            yield
+        end
+    else
+        if tag_exist_in_local? spec_version
+            `git push --tags`
+            yield
+        else
+            `git tag #{spec_version} && git push --tags`
+            yield
+        end
+    end
+end
+
 unless EXEC_FILE_NAME == "install.rb"
     if ['-v', '--version'].include? ARGV.first
         puts "latest version: #{version}" 
     else
-        file_path = podspec_file_path
-        if file_path
-            spec_name, spec_version = parse_spec_name_and_version_from_podspec file_path
-            if spec_name and spec_version
-                private_repo_name = repo_name_from_exec_name
-                if update_repo_ok? private_repo_name
-                    repo_all_path = File.join(Dir.home, ".cocoaPods", "repos", private_repo_name, "Specs", spec_name, spec_version)
-                    unless Dir.exist? repo_all_path 
-                        git_user_name = `git config user.name`
-                        message = "- [Add] #{spec_name} #{spec_version} by #{git_user_name}"
-                        commit_and_push file_path, repo_all_path, message
-                    else
-                        puts "ERR:: The version #{spec_version} of #{spec_name} is existed."
+        get_podspec_file_path do |file_path|
+            parse_spec_name_and_version_from_podspec file_path do |spec_name, spec_version|
+                push_spec_version_tag spec_version do
+                    update_repo repo_name_from_exec_name do |repo_name|
+                        repo_all_path = File.join(Dir.home, ".cocoaPods", "repos", repo_name, "Specs", spec_name, spec_version)
+                        unless Dir.exist? repo_all_path 
+                            git_user_name = `git config user.name`
+                            message = "- [Add] #{spec_name} #{spec_version} by #{git_user_name}"
+                            commit_and_push file_path, repo_all_path, message
+                        else
+                            puts "ERR:: The version #{spec_version} of #{spec_name} is existed."
+                        end
                     end
                 end
             end
